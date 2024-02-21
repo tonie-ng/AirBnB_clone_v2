68497c93b780f8cc7593e4476716cf7fa23c842a
Tonie Victor <victornwanochi@gmail.com>
2024-02-21 08:19:17 -0800

    welp++

---
+++ models/engine/file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+
+
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+
+        if cls:
+            all_obj = self.__objects
+            cls_obj = {}
+            for key in all_obj:
+                split_key = key.split('.')
+                if split_key[0] == cls.__name__:
+                    cls_obj[key] = self.__objects[key]
+            return cls_obj
+        else:
+            return self.__objects
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        del self.__objects[obj_key]
