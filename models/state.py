b3ebf551705ddda7068282f131904abbd27a8d02
Tonie Victor <victornwanochi@gmail.com>
2024-02-21 17:25:29 -0800

    minor

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
+import os
 from models.city import City
 
 
