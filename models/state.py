a1c6c53ce127d0d1853c628667962d1f99899519
Tonie Victor <victornwanochi@gmail.com>
2024-02-21 17:28:28 -0800

    trial...

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
-import os
 from models.city import City
 
 
@@ -12,28 +11,26 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-                "City",
-                back_populates="state",
-                cascade='all,delete,delete-orphan',
-                )
+    cities = relationship(
+            "City",
+            back_populates="state",
+            cascade='all,delete,delete-orphan',
+            )
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'file':
-        @property
-        def cities(self):
-            """
-            returns the list of City instances with
-            state_id equals to the current State.id
-            """
-            all_models = models.storage.all()
-            semi = []
-            final = []
-            for key in all_models:
-                city_class = key.split('.')
-                if city_class[0] == 'City':
-                    semi.append(all_models[key])
-            for item in semi:
-                if item.state_id == self.id:
-                    final.append(items)
-            return final
+    @property
+    def cities(self):
+        """
+        returns the list of City instances with
+        state_id equals to the current State.id
+        """
+        all_models = models.storage.all()
+        semi = []
+        final = []
+        for key in all_models:
+            city_class = key.split('.')
+            if city_class[0] == 'City':
+                semi.append(all_models[key])
+        for item in semi:
+            if item.state_id == self.id:
+                final.append(items)
+        return final
