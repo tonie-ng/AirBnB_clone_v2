2caef00e8b48294a09729aa6ee52345b21b10577
Tonie Victor <victornwanochi@gmail.com>
2024-03-07 17:38:27 -0800

    Keep it clean!

---
+++ 100-clean_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Deletes out-of-date archives
+"""
+
+from fabric.api import *
+import os
+
+env.hosts = ['100.26.227.216', '3.84.239.219']
+
+
+def do_clean(number=0):
+    """
+    deletes out-of-date archives
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
