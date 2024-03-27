a4deb9d582bb3f2f660131399df8d07bc21489e8
Tonie Victor <victornwanochi@gmail.com>
2024-03-26 19:38:18 -0700

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Hello world route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """HBNB route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """C route"""
+    param = text.replace("_", " ")
+    return "C {}".format(param)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Python route"""
+    param = text.replace("_", " ")
+    return "Python {}".format(param)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
