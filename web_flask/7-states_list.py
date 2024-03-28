1ddeaaa9c9f8bedce143f9c49295c70520254ea8
Tonie Victor <victornwanochi@gmail.com>
2024-03-27 18:53:02 -0700

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a Flask web application:"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """ list of all State objects present in DBStorage"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_app(ctx):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
