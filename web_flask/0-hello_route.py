b9264ca07f58ac9561ecaa14bb02024594c6bc30
Tonie Victor <victornwanochi@gmail.com>
2024-04-17 10:37:22 +0000

    Set up development with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_route():
     """Hello world route"""
     return "Hello HBNB!"
